cb7d64fadc51e3c6fad6e31ac3ec99d715f91848
daryatalantseva <d.talantseva@gmail.com>
2022-08-04 16:45:24 +0300

    Privet Masha

---
+++ exemple005_osoby_privet/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ВВедите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+    {
+        Console.WriteLine("Ура, это же Маша!!!");
+    }
+else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }    
